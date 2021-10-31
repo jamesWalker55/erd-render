7ca8698fcd591939aa328d19020e96809add691c
James Walker <james.chunho+github@gmail.com>
2021-10-31 12:09:38 +0000

    Create new diagram that renders diagrams from vertabelo.com

---
+++ diagrams/vertabelo.py
@@ -0,0 +1,61 @@
+from helpers import quick_entity, parse_attributes
+from modules.obj import Relation, COUNT
+from style.chen import render
+
+# https://vertabelo.com/blog/chen-erd-notation/
+
+prod = quick_entity(
+    "PRODUCT",
+    [
+        "NAME",
+        "*SKU",
+        "PRICE EXCL. VAT",
+        "VAT RATE",
+        "~PRICE INCL. VAT",
+    ],
+)
+
+student = quick_entity(
+    "STUDENT",
+    [
+        "*STUDENT ID",
+        "LAST NAME",
+        "FIRST NAME",
+        "~AGE",
+        "DATE OF BIRTH",
+        "EMAIL[]",
+        "MAJOR",
+        "ADDRESS: COUNTRY STATE CITY ZIPCODE STREETNAME STREETNO APARTMENTNO",
+    ],
+)
+
+book = quick_entity(
+    "BOOK",
+    [
+        "*BOOK ID",
+        "TITLE",
+    ],
+)
+
+chapter = quick_entity(
+    "CHAPTER",
+    [
+        "+CHAPTER ID",
+        "TITLE",
+    ],
+)
+
+contains = Relation(
+    (book, 1),
+    (chapter, COUNT.ANY),
+    name="CONTAINS",
+    is_identifying=True,
+)
+
+if __name__ == "__main__":
+    render(
+        [prod, student, book, chapter],
+        [contains],
+        filename="vertabelo.gv",
+        use_neato=True
+    )

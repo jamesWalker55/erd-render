0b6e27b7c437631af7413c00385d6aded8fdda13
James Walker <james.chunho+github@gmail.com>
2021-10-31 17:49:30 +0000

    Create setup.py to make this installable by pip

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="erd_render",
+    version="0.1",
+    description="A package that lets you declare entity-relationship diagrams programmatically and render them with graphviz",
+    url="http://github.com/",
+    author="James Walker",
+    author_email="james.chunho@gmail.com",
+    license="MIT",
+    packages=["erd_render"],
+    zip_safe=False,
+)
